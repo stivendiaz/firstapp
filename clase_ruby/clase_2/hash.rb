4512000e63145dce576a7a0448b364974a59652f
stivendiaz <stivendiazagudelo@gmail.com>
2018-02-21 19:28:45 -0500

    Agregamos archivos de lectura de archivos

---
+++ clase_ruby/clase_2/hash.rb
@@ -0,0 +1,4 @@
+persona = {nombre:"stiven",apellido:"diaz",edad:21}
+
+puts persona[nombre]
+persona.each{|x,y| puts "#{x}:#{y}"}
---
+++ clase_ruby/clase_2/retopegar.rb
@@ -0,0 +1,3 @@
+File.write("texto2.txt", File.read("texto.txt"))
+
+puts File.read("texto2.txt")
